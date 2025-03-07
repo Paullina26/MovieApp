2c0323cfcd7a3f957f7a0bb7338623a8041aa1d1
Paulina Golenia <70859448+Paullina26@users.noreply.github.com>
2025-03-07 15:46:16 +0100

    configuration tailwind, vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <>
-      <div>Film app</div>
+      <div className='min-h-screen bg-red-500 text-red flex items-center justify-center'>
+        <h1 className='text-3xl'>Tailwind TEST!</h1>
+      </div>
     </>
   );
 }
